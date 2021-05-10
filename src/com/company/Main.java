8dd6c4c0677e9dd3e54aa083165e4e20b15b40c3
gizem <gizemsacal@gmail.com>
2021-05-10 16:02:26 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,23 @@
+package com.company;
+
+import java.util.HashMap;
+
+class Main {
+    public static void main(String[] args) {
+
+        HashMap<Integer, String> languages = new HashMap<>();
+        languages.put(1, "Java");
+        languages.put(2, "Python");
+        languages.put(3, "JavaScript");
+        System.out.println("HashMap: " + languages);
+
+        // remove element associated with key 2
+        String value = languages.remove(2);
+        System.out.println("Removed value: " + value);
+
+        System.out.println("Updated HashMap: " + languages);
+    }
+    /* Burada remove()yöntem,
+     anahtarı parametresi olarak alır .
+      Daha sonra anahtarla ilişkili değeri döndürür ve girişi kaldırır .*/
+}
